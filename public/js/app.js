d60109b06a7a1dced041aa6d6cb09a99365df55f
Neil Sanghrajka <neilsanghrajka@outlook.com>
2016-05-17 17:51:37 -0700

    Added timestamp to messages

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,13 @@ var socket =io();
 socket.on('connect',function(){
 	console.log('Connected to socket io server');
 });
-socket.on('message',function(message){
-	console.log('New message: '+message.text);
 
-	//for class start with .
-	jQuery('.messages').append('<p>'+ message.text+'</p>');
+socket.on('message',function(message){
+	var momentTimestamp=moment.utc(message.timestamp);
+	console.log('New message');
+	console.log(message);
+
+	jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mm a')+'</strong>: '+message.text+'</p>');
 });
 
 // Handling submitting of new message. Start with pund for ids, money sign talks about jquery instance
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express =require('express');
 var app = express();
 var http = require('http').Server(app);//start a new server and use express app as boiler plate
 var io =require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname+'/public'));
 
@@ -10,14 +11,17 @@ io.on('connection',function(socket){
 	console.log('User connected via socket.io!');
 
 	socket.on('message',function(message){
-		console.log('Message receieved: '+message.text);
+		console.log(message.timestamp+': Message receieved: '+message.text);
 		
 		//socket.broadcast.emit('message',message);  send it to all except us
+		message.timestamp=moment().valueOf();
 		io.emit('message',message);
 	});
 
+	//timestamp property = javascript timestamp
 	socket.emit('message',{
-		text:'Welcome to to chat application!'
+		text:'Welcome to to chat application!',
+		timestamp:moment.valueOf()
 	});
 });
 
